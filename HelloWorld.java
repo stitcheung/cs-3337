556257ad955ad6f463bcb69d566f162afd7066f0
stitcheung <stitcheung@gmail.com>
2017-09-27 18:54:26 -0700

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Melissa Cheung");
+		System.out.println("This is Cs 3337 class");
+
+	}
+
+}
